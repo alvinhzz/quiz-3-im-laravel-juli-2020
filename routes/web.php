814cd631f15dde08f91c23a2e35fc9c38e14b32e
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2020-08-09 22:26:44 +0700

    finished templating

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',);
 
 Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
 Route::post('/items', 'ItemController@store'); // menyimpan data
